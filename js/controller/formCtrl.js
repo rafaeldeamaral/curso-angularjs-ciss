b86318f35718894269e5e7daea3fd2e648d118f7
CISSTESTE <rafadeamaral@bol.com.br>
2016-03-15 18:14:59 -0300

    Inclusão do angularJS

---
+++ js/controller/formCtrl.js
@@ -0,0 +1,11 @@
+myApp.controller('FormCtrl', function( $scope ) {
+	
+	$scope.pessoa = {
+		name : 'Rafael',
+		lastname : 'Amaral',
+		age : 29,
+		email : 'rafael@mail.com',
+		phone : 99999
+	}
+
+});
